c790e5de0f86b05cadd7b0aedff666e066c5d695
unknown <jharth@.leofra.ssc-ger.de>
2010-12-28 12:24:12 +0100

    * Do not generate a QR code when the user presses cancel * some rearrangements to for more stable JavaScript execution

--- src/main/javascript/qr-code-bookmarlet.js
+++ src/main/javascript/qr-code-bookmarlet.js
@@ -1,10 +1,16 @@
-"use strict";
-(function () {
-    var window = window,
+/*jslint white: true, browser: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
+(function (document) {
+    //noinspection BadExpressionStatementJS
+    "use strict";
+    var window = document.defaultView || document.parentWindow,
         url = window.prompt("Enter URL to encode", window.location),
         encodedUrl = encodeURIComponent(url),
         chartUrl = "http://chart.apis.google.com/chart?cht=qr&chs=400x400&chl=" + encodedUrl,
-        popUpName = "Qr code",
+        popUpName = "QR Code",
         popUpFeatures = "top=100,left=200,width=450,height=450";
+    if (null === url)
+    {
+        return;
+    }
     window.open(chartUrl, popUpName, popUpFeatures);
-}());
+}(document));
